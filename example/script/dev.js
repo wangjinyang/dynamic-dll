a911908609f8f8a30415c60387c52f5d05365033
liximomo <liximomo@gmail.com>
2022-04-08 18:31:33 +0800

    chore: update example

--- example/script/dev.js
+++ example/script/dev.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const lodash = require("lodash");
 const webpack = require("webpack");
 const devMiddleware = require("webpack-dev-middleware");
 const hotMiddleware = require("webpack-hot-middleware");
@@ -8,12 +7,9 @@ const express = require("express");
 const { DynamicDll } = require("../..");
 const app = express();
 
-const dllConfig = lodash.cloneDeep(baseConfig);
-dllConfig.plugins = [];
 const dynamicDll = new DynamicDll({
   webpackPath: path.join(path.dirname(require.resolve("webpack/package.json"))),
   exclude: [/webpack-hot-middleware\/client/, /react-refresh/],
-  dllWebpackConfig: dllConfig,
 });
 
 const config = dynamicDll.modifyWebpack(baseConfig);
